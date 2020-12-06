0a73f266519da77816f2a6db65f526cd860e1e94
Ajin28 <amisha28jain@gmail.com>
2020-12-07 02:30:36 +0530

    Node Modules: Callbacks and Error Handling

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ var rect = require('./rectangle')
 
 function solveRect(l, b) {
     console.log("Lenght =" + l + " and Breadth=" + b)
-    if (l <= 0 || b <= 0) {
-        console.log("Dimensions cannot be zero")
-    }
-    else {
-        console.log("Area =" + rect.area(l, b) + "\nPerimeter =" + rect.perimeter(l, b))
-    }
 
-    console.log()
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: " + err.message)
+        }
+        else {
+            console.log(`Area =` + rectangle.area() + ` where l=${l},b=${b}`)
+            console.log("Perimeter =" + rectangle.perimeter() + ` where l=${l},b=${b}`)
+        }
+        //Now, notice that for this area, I am not sending in 
+        //any parameters here because those two values, 
+        //the length and the breath have been passed in already in rect
+        //here in the l and b, and those would be already 
+        //available to this and here because of the closure that JavaScript supports
+
+        //this part will execute after the asynchronous (2 sec delay)
+
+    });
+
+    console.log("This is after the call to rect")
 }
 
 solveRect(2, 4)
--- rectangle.js
+++ rectangle.js
@@ -1,8 +1,25 @@
 //exports.perimeter = (x, y) => (2 * (x + y))
 //exports.area = (x, y) => (x * y)
 //or
-module.exports = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-};
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        //asynchoronous simulation
+        setTimeout(() =>
+            callback(new Error("Dimensions cannot be zero:" + ` l=${x}, b=${y}`), null)
+            , 2000);
+    }
+    else {
+        setTimeout(() =>
+            callback(null, {
+
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+                //I don't need to supply this x and y parameters here at all 
+                //because those x and y will be retrieved from this x and y supplied above
+
+
+            }), 2000);
+    }
+}
+
 

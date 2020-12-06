8545285a710edf35cfc321ab612fa0930ed59635
Ajin28 <amisha28jain@gmail.com>
2020-12-06 18:20:27 +0530

    Node Module Example (File-Based)

---
+++ rectangle.js
@@ -0,0 +1,8 @@
+//exports.perimeter = (x, y) => (2 * (x + y))
+//exports.area = (x, y) => (x * y)
+//or
+module.exports = {
+    perimeter: (x, y) => (2 * (x + y)),
+    area: (x, y) => (x * y)
+};
+

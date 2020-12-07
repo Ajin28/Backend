5a4f9e737bb571392a6f722666b92d70804827d9
Ajin28 <amisha28jain@gmail.com>
2020-12-07 10:00:46 +0530

    Node and the HTTP Module Example 1

--- node-http/index.js
+++ node-http/index.js
@@ -1,13 +1,51 @@
-const http = require("http");
+//Core Modules
+const http = require("http"); //http : the HTTP module is used to configure and start up a simple web server.
+const fs = require("fs") //file system :The file system module allows you to read and write files from your local file system
+const path = require("path") //path :The path allows you to specify the part of a file in your local file system
 
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end("<html><body><h1>hello world!</h1></body></html>")
+    console.log("Request for " + req.url + " by method " + req.method);
+
+    if (req.method == 'GET') {
+        var fileUrl;
+        if (req.url == "/") fileUrl = "/index.html"
+        else fileUrl = req.url
+
+        var filePath = path.resolve("./public" + fileUrl)
+        const fileExt = path.extname(filePath)
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader("Content-Type", 'text/html')
+                    res.end("<html><body><h1>Error 404:" + fileUrl + " not found</h1></body></html>")
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader("Content-Type", 'text/html')
+                fs.createReadStream(filePath).pipe(res)
+                //createReadStream method will read in the file from this filePath 
+                //and then convert that into stream of bytes, 
+                //and then we will pipe this through to the response.
+                // So that will be included into the response, in the body of the response
+            })
+
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", 'text/html')
+            res.end("<html><body><h1>Error 404:" + fileUrl + " not an HTML file</h1></body></html>")
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", 'text/html')
+        res.end("<html><body><h1>Error 404:" + req.method + " not supported</h1></body></html>")
+
+    }
 })
 
 server.listen(port, hostname, () => {

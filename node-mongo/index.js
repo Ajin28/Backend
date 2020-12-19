1b525a74916400e8f1509ad57b9d08c224417cee
Ajin28 <amisha28jain@gmail.com>
2020-12-19 21:02:57 +0530

    mongod callback vs promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,4 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// We know that the node MongoDB driver natively supports promises.
+// So, if you do not specify a callback, the calls to their functions will return promises.
+
+
+const MongoClient = require('mongodb').MongoClient
 const assert = require("assert");
 //The assert module enables us to use the assert to check for truth or false values within our application
 const dboper = require("./operations")
@@ -11,32 +15,35 @@ const dbname = "conFusion";
 // second parameter is a callback function
 // the callback function has two parameters, the error value, and client, which we can then use to connect to our database and then perform various operations.
 
-MongoClient.connect(url, (err, client) => {
-    assert.strictEqual(err, null)
-    console.log("Connected correctly to server")
-    const db = client.db(dbname);
+MongoClient.connect(url)
+    .then((client) => {
+        console.log("Connected correctly to server")
+        const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes', (result) => {
-        console.log('Insert Documet:\n', result.ops);
-
-        dboper.findDocument(db, 'dishes', (docs) => {
-            console.log("Found Documents:\n", docs);
-
-            dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, 'dishes', (result) => {
-                console.log('Updated Document:\n', result.result);
-
-                dboper.findDocument(db, 'dishes', (docs) => {
-                    console.log("Found Updated Documents:\n", docs);
-
-                    db.dropCollection('dishes', (result) => {
-                        console.log("Dropped Collection: ", result);
-                    })
-                })
+        dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, 'dishes')
+            .then((result) => {
+                console.log('Insert Documet:\n', result.ops);
+                return dboper.findDocument(db, 'dishes')
             })
-        })
+            .then((docs) => {
+                console.log("Found Documents:\n", docs);
+                return dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, 'dishes')
+            })
+
+            .then((result) => {
+                console.log('Updated Document:\n', result.result);
+                return dboper.findDocument(db, 'dishes')
+            })
+            .then((docs) => {
+                console.log("Found Updated Documents:\n", docs);
+                return db.dropCollection('dishes')
+            })
+            .then((result) => {
+                console.log("Dropped Collection: ", result);
+                return client.close()
+            })
+            .catch(err => { console.log(err); })
     })
-
-})
-
+    .catch(err => { console.log(err); })
 
 
